e22057362844b5fa9f410a8f97e0187f85ad175d
Max <user_@server.fake>
2013-06-19 18:56:01 +0000

    create the js skeleton

---
+++ app.js
@@ -0,0 +1,28 @@
+
+function readMAC() {
+    return 0
+}
+
+function readMPD() {
+    return 0
+}
+
+function readStatus() {
+    return 0
+}
+
+function readAmbiance() {
+    return 0
+}
+
+function readPlaylist() {
+    return 0
+}
+
+$(document).ready(function(){
+    readMAC();
+    readMPD();
+    readStatus();
+    readAmbiance();
+    readPlaylist(); 
+});
